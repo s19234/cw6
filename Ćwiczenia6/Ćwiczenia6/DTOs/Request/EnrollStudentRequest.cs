90b54dd11b446a430e7d0e844bbd529bb7cd0496
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 11:21:51 +0200

    Dodano komentarze

--- Ćwiczenia6/Ćwiczenia6/DTOs/Request/EnrollStudentRequest.cs
+++ Ćwiczenia6/Ćwiczenia6/DTOs/Request/EnrollStudentRequest.cs
@@ -6,22 +6,44 @@ using System.Threading.Tasks;
 
 namespace Ćwiczenia6.DTOs.Request
 {
+    /// <summary>
+    /// Klasa wykorzystywana do dodawania nowego studenta do bazy danych
+    /// </summary>
     public class EnrollStudentRequest
     {
+        /// <summary>
+        /// Indeks studenta
+        /// </summary>
         public string IndexNumber { get; set; }
+
+        /// <summary>
+        /// Email studenta
+        /// </summary>
         public string Email { get; set; }
         
+        /// <summary>
+        /// Imię studenta - nie puste, nie dłuższe niż 10 znaków
+        /// </summary>
         [Required(ErrorMessage = "Musisz podać imię")]
         [MaxLength(10)]
         public string FirstName { get; set; }
         
+        /// <summary>
+        /// Nazwisko studenta - nie puste, nie dłuższe niż 255 znaków
+        /// </summary>
         [Required]
         [MaxLength(255)]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Data urodzenia studenta
+        /// </summary>
         public DateTime BirthDate { get; set; }
 
+        /// <summary>
+        /// Wydział studiów
+        /// </summary>
         [Required]
-        public string Students { get; set; }
+        public string Studies { get; set; }
     }
 }
