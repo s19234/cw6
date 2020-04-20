c2e6caae71c3281c825a17a3a58a9c7519f85008
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 11:19:01 +0200

    Dodano klasę EnrollStudentRequest

---
+++ Ćwiczenia6/Ćwiczenia6/DTOs/Request/EnrollStudentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ćwiczenia6.DTOs.Request
+{
+    public class EnrollStudentRequest
+    {
+        public string IndexNumber { get; set; }
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Musisz podać imię")]
+        [MaxLength(10)]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public string Students { get; set; }
+    }
+}
