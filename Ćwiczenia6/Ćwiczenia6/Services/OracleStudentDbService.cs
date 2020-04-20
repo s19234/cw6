833ec5fd8e7b7c828caab1577a9289c1bcf30a67
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 11:43:27 +0200

    Dodano klase OracleStudentDbService.cs

---
+++ Ćwiczenia6/Ćwiczenia6/Services/OracleStudentDbService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ćwiczenia6.DTOs.Request;
+
+namespace Ćwiczenia6.Services
+{
+    /// <summary>
+    /// Klasa do manipulacji bazą danych stworzoną w Oracle
+    /// </summary>
+    public class OracleStudentDbService : IStudentDbService
+    {
+        public void EnrollStudent(EnrollStudentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PromoteStudent(int semester, string studies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
