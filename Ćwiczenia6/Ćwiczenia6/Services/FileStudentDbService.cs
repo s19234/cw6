55bea498797e4299cf040440feef99c44f4861f2
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 11:40:57 +0200

    Dodano klasę FileStudentDbService.cs

---
+++ Ćwiczenia6/Ćwiczenia6/Services/FileStudentDbService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ćwiczenia6.DTOs.Request;
+
+namespace Ćwiczenia6.Services
+{
+    /// <summary>
+    /// Pusta klasa potrzebna na nowe projekty
+    /// </summary>
+    public class FileStudentDbService : IStudentDbService
+    {
+        public void EnrollStudent(EnrollStudentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PromoteStudent(int semester, string studies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
