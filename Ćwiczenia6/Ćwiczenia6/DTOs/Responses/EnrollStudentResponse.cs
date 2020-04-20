70d8f2ad6dd4cf41f26ad3916de3ed8d1a10cf3a
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 11:25:50 +0200

    Dodano klasę EnrollStudentResponse

---
+++ Ćwiczenia6/Ćwiczenia6/DTOs/Responses/EnrollStudentResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ćwiczenia6.DTOs.Responses
+{
+    /// <summary>
+    /// Odpowiedź z bazy danych
+    /// </summary>
+    public class EnrollStudentResponse
+    {
+        /// <summary>
+        /// Nazwisko studenta
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Aktualny semestr studenta
+        /// </summary>
+        public int Semester { get; set; }
+
+        /// <summary>
+        /// Data rozpoczęcia studiów
+        /// </summary>
+        public DateTime StartDate { get; set; }
+    }
+}
