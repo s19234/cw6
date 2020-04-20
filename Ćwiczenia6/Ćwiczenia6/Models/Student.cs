c3be7e5648ee0dfe67f927b30551bc2b353ab22f
Ryszard <s19234@pjwskt.edu.pl>
2020-04-20 11:30:06 +0200

    Dodano klasę Student

---
+++ Ćwiczenia6/Ćwiczenia6/Models/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ćwiczenia6.Models
+{
+    /// <summary>
+    /// Klasa przechowująca dane o studencie
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Indeks studenta
+        /// </summary>
+        public string IndexNumber { get; set; }
+
+        /// <summary>
+        /// Imię studenta
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Nazwisko studenta
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Data urodzin studenta
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// Wydział
+        /// </summary>
+        public string Studies { get; set; }
+
+        /// <summary>
+        /// Aktualny semestr studenta
+        /// </summary>
+        public int Semester { get; set; }
+    }
+}
